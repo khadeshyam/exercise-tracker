584d1027c80155a7d610064c7ae5b5eed920ee5c
SHYAM KHADE <khade.shyam10.1@gmail.com>
2023-02-16 13:12:06 +0530

    copy files from project github repo

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config();
+const mongoose = require('mongoose');
+const userRouter = require('./routes/user');
+
+app.use(express.urlencoded({extended:true}));
+app.use(cors())
+app.use(express.static('public'));
+
+mongoose.set('strictQuery',true);
+
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+})
+mongoose.connection.on('connected',()=>{
+    console.log('conneted to mongo yeahh');
+})
+mongoose.connection.on('error',(err)=>{
+    console.log('err connecting',err)
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+});
+
+app.use(userRouter);
+
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
