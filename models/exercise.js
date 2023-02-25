7da1f5532a1121ba961d50ac61b57ad60f9ac821
SHYAM KHADE <khade.shyam10.1@gmail.com>
2023-02-25 19:53:25 +0530

    complete the challenge

---
+++ models/exercise.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const exerciseSchema = new mongoose.Schema({
+    description:{
+      type:String,
+      required:true,
+    },
+    duration:{
+      type:Number,
+      required:true,
+    },
+    date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = exerciseSchema;
--- models/user.js
+++ models/user.js
@@ -1,12 +1,21 @@
 const mongoose = require('mongoose');
+const exerciseSchema = require('./exercise');
+
+console.log(Exercise);
 
 const userSchema = new mongoose.Schema({
-     username:{
-        type:String,
-        required:true,
-     }
+  username: {
+    type: String,
+    required: true,
+  },
+   count:{
+     type: Number,
+     default: 0,
+  },
+  logs:[exerciseSchema]
 })
 
-const User = new mongoose.model("User",userSchema);
+const User = new mongoose.model("User", userSchema);
 
 module.exports = User;
+
